59af149c416ca39ef1ab57c825cfc7fe6e29b09c
Jayakrishnan J <simplejeykey@gmail.com>
2024-07-13 19:45:06 +0530

    Program to compute the heights of the bounces of a ball dropped from 100 metres. It bounces only 3/5 of its last height every next bounce.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # The ball is dropped from 100 metres.
+rebound_factor = 3/5 # The ball bounces only 3/5 its orignal height every bounce.
+
+for i in range(10):
+    print(round(height*rebound_factor, 4)) # round function to round to the 4th decimal place.
+    height = height*rebound_factor
