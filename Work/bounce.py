76358e346422bbc8d4e919e5585e59446e6e82cc
Jayakrishnan J <simplejeykey@gmail.com>
2024-07-13 21:37:37 +0530

    Added comments to the program

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,9 +2,12 @@
 #
 # Exercise 1.5
 
-height = 100 # The ball is dropped from 100 metres.
-rebound_factor = 3/5 # The ball bounces only 3/5 its orignal height every bounce.
+height = 100 #  The ball is dropped from 100 metres.
+rebound_factor = 3/5 #  The ball bounces only 3/5 
+#  its orignal height every bounce.
 
 for i in range(10):
-    print(round(height*rebound_factor, 4)) # round function to round to the 4th decimal place.
+    print(round(height*rebound_factor, 4)) #  round function to round to
+#  the 4th decimal place.
     height = height*rebound_factor
+    
