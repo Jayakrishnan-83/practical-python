9df17e9e48cadd87e2848719c57366884fc5ea28
Jayakrishnan J <simplejeykey@gmail.com>
2024-07-15 11:00:19 +0530

    Program to calculate the total cost of shares

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+file = open('Data/portfolio.csv', 'rt')
+headers = next(file).split(',')
+
+total_cost = 0
+
+for line in file:
+    row = line.split(',')
+    cost_of_shares = int(row[1])*float(row[2])
+    total_cost += cost_of_shares
+
+print(total_cost)
+
+file.close()
